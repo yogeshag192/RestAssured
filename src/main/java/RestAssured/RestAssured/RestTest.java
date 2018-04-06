dd0631f44866d9aff017c3349686c536e8a702ef
yogeshag192 <yogesh.ag90@gmail.com>
2018-04-06 20:22:33 +0530

    commit 1

--- src/main/java/RestAssured/RestAssured/RestTest.java
+++ src/main/java/RestAssured/RestAssured/RestTest.java
@@ -1,19 +1,48 @@
 package RestAssured.RestAssured;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
+import org.json.simple.JSONObject;
 import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
 
 public class RestTest {
 	
-	
-	public static void main(String args[]) {
+	@Test
+	public void checkStatus200() {
 	        
-	  given().when().get("https://jsonplaceholder.typicode.com/posts/1");
+	  
+	given().get("http://services.groupkt.com/country/get/iso2code/IN").then().statusCode(200).log().all();
 	   
 	   
 	   /*     assertThat().
 	        body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));*/
+	}
+	
+	@Test
+	public void test2(){
+		RequestSpecification req = RestAssured.given();
+		req.header("content-type", "application/json");
+		JSONObject json = new JSONObject();
+		json.put("userID", "11");
+		json.put("id", "102");
+		json.put("title", "yogesh");
+		json.put("body", "test api2");
+		
+		req.body(json.toJSONString());
+		
+		Response resp = req.post("https://jsonplaceholder.typicode.com/posts/");
+		System.out.println("Post Response");
+		System.out.println(resp.asString());
+		int statusCode = resp.getStatusCode();
+		System.out.println("StatusCode: " +statusCode);
+		Assert.assertEquals(statusCode, 202);
+		
+		
 	}
 	
 	
